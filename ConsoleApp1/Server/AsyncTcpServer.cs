227baabc7cf80d532b476cdff2e229dcb331b869
twonine <1185366392@qq.com>
2025-02-23 20:53:33 +0800

    1. 优化线程池数据结构，添加活跃时间的记录 2. 增加服务端清理不活跃连接功能

--- ConsoleApp1/Server/AsyncTcpServer.cs
+++ ConsoleApp1/Server/AsyncTcpServer.cs
@@ -12,7 +12,8 @@ public class AsyncTcpServer
 {
     private readonly Socket _listener;
     private readonly int _port;
-    private readonly ConcurrentDictionary<string,Socket> _clients = new();
+    //创建一个线程安全的字典，key为guid，value为一个元组， 元组的元素为socket链接以及最后活跃时间
+    private readonly ConcurrentDictionary<string, (Socket client, DateTime lastActiveTime)> _clients = new();
     private readonly bool _enableHeartbeat;
 
     public AsyncTcpServer(int port, bool enableHeartbeat=false)
@@ -29,14 +30,15 @@ public class AsyncTcpServer
         //开始监听，最大连接数为100
         _listener.Listen(100);
         Console.WriteLine($"[Server] Listening on port {_port}");
+        _ = Task.Run(CleanupInactiveClients);
 
         //循环接收连接
         while (true)
         {
             var client = await _listener.AcceptAsync();
             Console.WriteLine($"[Server] Client connected: {client.RemoteEndPoint}");
-            //为连接开启一个异步处理线程
-            _ = HandleClientAsync(client);
+            //为连接开启一个独立异步处理线程
+            _ = Task.Run(() => HandleClientAsync(client));
         }
     }
 
@@ -46,8 +48,8 @@ public class AsyncTcpServer
     /// <param name="client"></param>
     private async Task HandleClientAsync(Socket client)
     {
-        string clientId=client.RemoteEndPoint?.ToString()??Guid.NewGuid().ToString();//防止RemoteEndPoint为空
-        _clients[clientId] = client;
+        string clientId = client.RemoteEndPoint?.ToString() ?? Guid.NewGuid().ToString(); //防止RemoteEndPoint为空
+        _clients[clientId] = (client, DateTime.UtcNow);
         try
         {
             byte[] buffer = new byte[4];//消息长度
@@ -74,11 +76,12 @@ public class AsyncTcpServer
                 byte[] msgBuffer = new byte[msgLength];
 
                 //msgBuffer中存放的是客户端发送的完整的包
-                await ReceiveExactAsync(client, msgBuffer, msgLength);
+                await ReceiveExactAsync(client, msgBuffer.AsMemory(), msgLength);
                 string msg = Encoding.UTF8.GetString(msgBuffer);
                 if ("[HEARTBEAT]".Equals(msg))
                 {
                     Console.WriteLine($"[Server] [HEARTBEAT] from {clientId}");
+                    _clients[clientId] = (client, DateTime.UtcNow);//更新最后活跃时间
                     continue;
                 }
                 Console.WriteLine($"[Server] Received from {clientId}: {msg}");
@@ -97,7 +100,10 @@ public class AsyncTcpServer
             {
                 client.Shutdown(SocketShutdown.Both);
             }
-            catch (Exception e){}
+            catch (SocketException e)
+            {
+                Console.WriteLine($"[Server] shutting down client Exception occured: {e}");
+            }
             client.Close();
             Console.WriteLine($"[Server] Client closed");
         }
@@ -111,12 +117,13 @@ public class AsyncTcpServer
     /// <param name="count"></param>
     /// <returns></returns>
     /// <exception cref="SocketException"></exception>
-    private async Task<int> ReceiveExactAsync(Socket socket, byte[] buffer, int count)
+    private async Task<int> ReceiveExactAsync(Socket socket, Memory<byte> buffer, int count)
     {
         int received = 0;
         while (received < count)
         {
-            int bytes = await socket.ReceiveAsync(buffer.AsMemory(received, count - received), SocketFlags.None);
+            //使用切片，避免手动计算偏移量
+            int bytes = await socket.ReceiveAsync(buffer[received..], SocketFlags.None);
             if (bytes == 0)
             {
                 throw new SocketException((int)SocketError.ConnectionReset);
@@ -127,11 +134,34 @@ public class AsyncTcpServer
     }
     private async Task SendMessageAsync(string clientId, string data)
     {
-        if (!_clients.TryGetValue(clientId, out var client)) return;
+        if (!_clients.TryGetValue(clientId, out var clientItem)) return;
         byte[] msgBytes=Encoding.UTF8.GetBytes(data);
         byte[] lengthBytes = BitConverter.GetBytes(msgBytes.Length);
 
-        await client.SendAsync(lengthBytes, SocketFlags.None);
-        await client.SendAsync(msgBytes, SocketFlags.None);
-    }   
+        await clientItem.client.SendAsync(lengthBytes, SocketFlags.None);
+        await clientItem.client.SendAsync(msgBytes, SocketFlags.None);
+    }
+
+    /// <summary>
+    /// 定期清理长时间不活跃的链接
+    /// </summary>
+    private async Task CleanupInactiveClients()
+    {
+        while (true)
+        {
+            await Task.Delay(TimeSpan.FromMinutes(1));
+            
+            var now = DateTime.UtcNow;
+            foreach (var item in _clients)
+            {
+                var (client, lastActiveTime) = item.Value;
+                if ((now - lastActiveTime).TotalSeconds >= 120)
+                {
+                    Console.WriteLine($"[Server] Removing inactive client:{item.Key} ");
+                    _clients.TryRemove(item.Key, out _);
+                    client.Close();
+                }
+            }
+        }
+    }
 }
